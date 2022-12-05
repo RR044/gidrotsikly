9acf64b4d197009f38bd99fcb19767627c46df28
Igor Konoval <rrww4477@gmail.com>
2022-12-05 23:18:27 +0200

    first commit

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -30,15 +30,18 @@ import Swiper from '../vendor/swiper';
 //   breakpointChecker();
 // })();
 
-    let twitSlider = new Swiper(".offer-swap", {
-    slidesPerView: 3,
+    let boatSlide = new Swiper(".boat-container", {
+    slidesPerView: 1,
     spaceBetween: 10,
     loop: true,
     observer: true,
     observeParents: true,
     navigation: {
-      nextEl: '.swiper-button-prev1',
-      prevEl: '.swiper-button-next1',
+      nextEl: '.swiper-button-prev',
+      prevEl: '.swiper-button-next',
+    },
+    pagination: {
+      el: '.boat-pagination'
     },
 
     breakpoints: {
@@ -52,90 +55,21 @@ import Swiper from '../vendor/swiper';
           },
           // mobile + tablet - 576-768
           576: {
-            slidesPerView: 2,
+            slidesPerView: 1,
             spaceBetween:10,
           },
           // desktop >= 768
           1160: {
-            slidesPerView: 3,
+            slidesPerView: 1,
             spaceBetween: 10,
           },
         },
 
-    
     });
 
-    let updateSlider = new Swiper(".update-swap", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      loop: true,
-      observer: true,
-      observeParents: true,
-      pagination: {
-        el: '.swiper-pagination2',
-      },
-      navigation: {
-        nextEl: '.swiper-button-prev2',
-        prevEl: '.swiper-button-next2',
-      },
-
-      breakpoints: {
-        // mobile - 120-414
-        120: {
-          slidesPerView: 1,
-        },
-         // mobile - 414-576
-         414: {
-          slidesPerView: 1,
-        },
-        // mobile + tablet - 576-768
-        576: {
-          slidesPerView: 2,
-          spaceBetween:10,
-        },
-        // desktop >= 768
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 10,
-        },
-      },
-  
-      
-      });
 
 
-  // let gallarySlider = new Swiper(".gallary-swap", {
-  // slidesPerView: 4,
-  // spaceBetween: 30,
-  // loop: true,
-  // observer: true,
-  // observeParents: true,
-  // navigation: {
-  // nextEl: '.swiper-button-prev3',
-  // prevEl: '.swiper-button-next3',
-  //   },
-    
-  //   breakpoints: {
-  //     // mobile - 320-414
-  //     320: {
-  //       slidesPerView: 1,
-  //     },
-  //      // mobile - 414-576
-  //      414: {
-  //       slidesPerView: 2,
-  //     },
-  //     // mobile + tablet - 576-768
-  //     576: {
-  //       slidesPerView: 3,
-  //       spaceBetween:20,
-  //     },
-  //     // desktop >= 768
-  //     768: {
-  //       slidesPerView: 4,
-  //       spaceBetween: 30,
-  //     },
-  //   },
-    
-    // });
+
+
     
 
