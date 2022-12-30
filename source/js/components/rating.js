c3c5c17ed041de76342aa13c8ed81364ff5e1efe
Igor Konoval <rrww4477@gmail.com>
2022-12-30 23:25:38 +0200

    bug fix

--- source/js/components/rating.js
+++ source/js/components/rating.js
@@ -1,33 +1,33 @@
-"use strict"
+// "use strict"
 
-const ratings = document.querySelectorAll('.rating');
+// const ratings = document.querySelectorAll('.rating');
 
-if (ratings.length > 0) {
-    initRatings();
-}
+// if (ratings.length > 0) {
+//     initRatings();
+// }
 
-function initRatings() {
-    let ratingActive, ratingValue;
-    for (let index = 0; index < ratings.length; index++) {
-        const rating = ratings[index];
-        initRating(rating);
-    }
+// function initRatings() {
+//     let ratingActive, ratingValue;
+//     for (let index = 0; index < ratings.length; index++) {
+//         const rating = ratings[index];
+//         initRating(rating);
+//     }
 
 
-    function initRatings(rating) {
-        initRatingVars(rating);
+//     function initRatings(rating) {
+//         initRatingVars(rating);
 
-        setRatingActiveWidth();
-    }
+//         setRatingActiveWidth();
+//     }
 
-    function initRatingVars(rating) {
-        ratingActive = rating.querySelector('.rating__active');
-        rating
-        Value = rating.querySelector('.rating__value');
-    }
+//     function initRatingVars(rating) {
+//         ratingActive = rating.querySelector('.rating__active');
+//         rating
+//         Value = rating.querySelector('.rating__value');
+//     }
 
-    function setRatingActiveWidth(index = ratingValue.innerHTML) {
-        const ratingActiveWidth = index / 0.05;
-        ratingActive.style.width = `${ratingActiveWidth}%`;
-    }
-}
+//     function setRatingActiveWidth(index = ratingValue.innerHTML) {
+//         const ratingActiveWidth = index / 0.05;
+//         ratingActive.style.width = `${ratingActiveWidth}%`;
+//     }
+// }
